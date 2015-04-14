1a172020e48fc47791a7e5bbe3e75df01951c6c4
Dan Rohr <dcr83@cornell.edu>
2015-04-14 19:11:19 -0400

    Increase error package test coverage

--- errors/error_test.go
+++ errors/error_test.go
@@ -18,6 +18,164 @@ func TestNew(t *testing.T) {
 	if err.Message != "Unknown certificate error" {
 		t.Fatal("Error message construction failed.")
 	}
+
+	code := New(OCSPError, ReadFailed).ErrorCode
+	if code != 8001 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(OCSPError, IssuerMismatch).ErrorCode
+	if code != 8100 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(OCSPError, InvalidStatus).ErrorCode
+	if code != 8200 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(CertificateError, Unknown).ErrorCode
+	if code != 1000 {
+		t.Fatal("Improper error code")
+	}
+	code = New(CertificateError, ReadFailed).ErrorCode
+	if code != 1001 {
+		t.Fatal("Improper error code")
+	}
+	code = New(CertificateError, DecodeFailed).ErrorCode
+	if code != 1002 {
+		t.Fatal("Improper error code")
+	}
+	code = New(CertificateError, ParseFailed).ErrorCode
+	if code != 1003 {
+		t.Fatal("Improper error code")
+	}
+	code = New(CertificateError, SelfSigned).ErrorCode
+	if code != 1100 {
+		t.Fatal("Improper error code")
+	}
+	code = New(CertificateError, VerifyFailed).ErrorCode
+	if code != 1200 {
+		t.Fatal("Improper error code")
+	}
+	code = New(CertificateError, BadRequest).ErrorCode
+	if code != 1300 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(PrivateKeyError, Unknown).ErrorCode
+	if code != 2000 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, ReadFailed).ErrorCode
+	if code != 2001 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, DecodeFailed).ErrorCode
+	if code != 2002 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, ParseFailed).ErrorCode
+	if code != 2003 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, Encrypted).ErrorCode
+	if code != 2100 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, NotRSAOrECC).ErrorCode
+	if code != 2200 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, KeyMismatch).ErrorCode
+	if code != 2300 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, GenerationFailed).ErrorCode
+	if code != 2400 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PrivateKeyError, Unavailable).ErrorCode
+	if code != 2500 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(IntermediatesError, Unknown).ErrorCode
+	if code != 3000 {
+		t.Fatal("Improper error code")
+	}
+	code = New(IntermediatesError, ReadFailed).ErrorCode
+	if code != 3001 {
+		t.Fatal("Improper error code")
+	}
+	code = New(IntermediatesError, DecodeFailed).ErrorCode
+	if code != 3002 {
+		t.Fatal("Improper error code")
+	}
+	code = New(IntermediatesError, ParseFailed).ErrorCode
+	if code != 3003 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(RootError, Unknown).ErrorCode
+	if code != 4000 {
+		t.Fatal("Improper error code")
+	}
+	code = New(RootError, ReadFailed).ErrorCode
+	if code != 4001 {
+		t.Fatal("Improper error code")
+	}
+	code = New(RootError, DecodeFailed).ErrorCode
+	if code != 4002 {
+		t.Fatal("Improper error code")
+	}
+	code = New(RootError, ParseFailed).ErrorCode
+	if code != 4003 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(PolicyError, Unknown).ErrorCode
+	if code != 5000 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PolicyError, NoKeyUsages).ErrorCode
+	if code != 5100 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PolicyError, InvalidPolicy).ErrorCode
+	if code != 5200 {
+		t.Fatal("Improper error code")
+	}
+	code = New(PolicyError, InvalidRequest).ErrorCode
+	if code != 5300 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(DialError, Unknown).ErrorCode
+	if code != 6000 {
+		t.Fatal("Improper error code")
+	}
+
+	code = New(APIClientError, AuthenticationFailure).ErrorCode
+	if code != 7100 {
+		t.Fatal("Improper error code")
+	}
+	code = New(APIClientError, JSONError).ErrorCode
+	if code != 7200 {
+		t.Fatal("Improper error code")
+	}
+	code = New(APIClientError, ClientHTTPError).ErrorCode
+	if code != 7400 {
+		t.Fatal("Improper error code")
+	}
+	code = New(APIClientError, IOError).ErrorCode
+	if code != 7300 {
+		t.Fatal("Improper error code")
+	}
+	code = New(APIClientError, ServerRequestFailed).ErrorCode
+	if code != 7500 {
+		t.Fatal("Improper error code")
+	}
 }
 
 func TestWrap(t *testing.T) {
