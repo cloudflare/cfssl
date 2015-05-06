9e7c731b90d0bd5ce77cb2914a7e36c63cec4d19
timestruck <mx48@cornell.edu>
2015-05-06 00:39:39 -0400

    improved generator test

--- api/generator/generator_test.go
+++ api/generator/generator_test.go
@@ -3,11 +3,13 @@ package generator
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/cloudflare/cfssl/config"
+	"github.com/cloudflare/cfssl/csr"
+	"github.com/cloudflare/cfssl/signer/local"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/cloudflare/cfssl/csr"
+	"time"
 )
 
 func csrData(t *testing.T) *bytes.Reader {
@@ -67,4 +69,62 @@ func TestGeneratorRESTfulVerbs(t *testing.T) {
 	if resp.StatusCode != http.StatusMethodNotAllowed {
 		t.Fatal(resp.Status)
 	}
+}
+
+type ResponseEx struct {
+}
+
+type errorString struct {
+	s string
+}
+
+func (e *errorString) Error() string {
+	return e.s
+}
+
+func (r *ResponseEx) Header() http.Header {
+	h := map[string][]string{
+		"rsc": []string{"hello", "you"},
+	}
+	return h
+}
+
+func (r *ResponseEx) Write([]byte) (int, error) {
+	return 2, &errorString{"error"}
+}
+
+func (r *ResponseEx) WriteHeader(int) {
+
+}
+
+func testValidator(req *csr.CertificateRequest) error {
+	return nil
+}
+
+func TestInvalidHTTP(t *testing.T) {
+	var expiry = 1 * time.Minute
+
+	var validSigning = &config.Signing{
+		Profiles: map[string]*config.SigningProfile{
+			"valid": {
+				Usage:  []string{"digital signature"},
+				Expiry: expiry,
+			},
+		},
+		Default: &config.SigningProfile{
+			Usage:  []string{"digital signature"},
+			Expiry: expiry,
+		},
+	}
+
+	signer2, err5 := local.NewSignerFromFile(testCaFile, testCaKeyFile, validSigning)
+	if err5 != nil {
+		t.Fatal(err5)
+	}
+
+	ghSigner := NewCertGeneratorHandlerFromSigner(CSRValidate, signer2)
+	if ghSigner == nil {
+		t.Fatalf("%v", ghSigner)
+	}
+
 }
