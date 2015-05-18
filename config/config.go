a42b0f1345c9cf4ec5cc505df45ee3e3355edef0
Nick Sullivan <nicholas.sullivan@gmail.com>
2015-05-18 14:02:21 -0700

    Add ability to set whitelist in config.json

--- config/config.go
+++ config/config.go
@@ -50,18 +50,20 @@ type CertificatePolicy struct {
 // A SigningProfile stores information that the CA needs to store
 // signature policy.
 type SigningProfile struct {
-	Usage          []string  `json:"usages"`
-	IssuerURL      []string  `json:"issuer_urls"`
-	OCSP           string    `json:"ocsp_url"`
-	CRL            string    `json:"crl_url"`
-	CA             bool      `json:"is_ca"`
-	OCSPNoCheck    bool      `json:"ocsp_no_check"`
-	ExpiryString   string    `json:"expiry"`
-	BackdateString string    `json:"backdate"`
-	AuthKeyName    string    `json:"auth_key"`
-	RemoteName     string    `json:"remote"`
-	NotBefore      time.Time `json:"not_before"`
-	NotAfter       time.Time `json:"not_after"`
+	Usage               []string  `json:"usages"`
+	IssuerURL           []string  `json:"issuer_urls"`
+	OCSP                string    `json:"ocsp_url"`
+	CRL                 string    `json:"crl_url"`
+	CA                  bool      `json:"is_ca"`
+	PolicyStrings       []string  `json:"policies"`
+	OCSPNoCheck         bool      `json:"ocsp_no_check"`
+	ExpiryString        string    `json:"expiry"`
+	BackdateString      string    `json:"backdate"`
+	AuthKeyName         string    `json:"auth_key"`
+	RemoteName          string    `json:"remote"`
+	NotBefore           time.Time `json:"not_before"`
+	NotAfter            time.Time `json:"not_after"`
+	NameWhitelistString string    `json:"name_whitelist"`
 
 	Policies      []asn1.ObjectIdentifier
 	Expiry        time.Duration
@@ -199,6 +201,16 @@ func (p *SigningProfile) populate(cfg *Config) error {
 			return cferr.Wrap(cferr.PolicyError, cferr.InvalidPolicy,
 				errors.New("failed to find auth_key in auth_keys section"))
 		}
+	}
+
+	if p.NameWhitelistString != "" {
+		log.Debug("compiling whitelist regular expression")
+		rule, err := regexp.Compile(p.NameWhitelistString)
+		if err != nil {
+			return cferr.Wrap(cferr.PolicyError, cferr.InvalidPolicy,
+				errors.New("failed to compile name whitelist section"))
+		}
+		p.NameWhitelist = rule
 	}
 
 	return nil
