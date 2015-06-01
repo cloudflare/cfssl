74c3a2d0ee2aa53060cd8bb64fa5b8d901d49879
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-06-01 11:53:15 -0700

    Fix go vet issues.

--- ocsp/pkcs11/pkcs11_stub.go
+++ ocsp/pkcs11/pkcs11_stub.go
@@ -11,7 +11,7 @@ import (
 // Enabled is set to true if PKCS #11 support is present.
 const Enabled = false
 
-// New returns a new PKCS #11 signer.
+// NewPKCS11Signer returns a new PKCS #11 signer.
 func NewPKCS11Signer(cfg ocspConfig.Config) (ocsp.Signer, error) {
 	return nil, errors.New(errors.PrivateKeyError, errors.Unavailable)
 }
--- ocsp/universal/universal.go
+++ ocsp/universal/universal.go
@@ -6,12 +6,12 @@ import (
 	"github.com/cloudflare/cfssl/ocsp/pkcs11"
 )
 
+// NewSignerFromConfig generates a new OCSP signer from a config object.
 func NewSignerFromConfig(cfg ocspConfig.Config) (ocsp.Signer, error) {
 	if cfg.PKCS11.Module != "" {
 		return pkcs11.NewPKCS11Signer(cfg)
-	} else {
-		return ocsp.NewSignerFromFile(cfg.CACertFile, cfg.ResponderCertFile,
-			cfg.KeyFile, cfg.Interval)
 	}
+	return ocsp.NewSignerFromFile(cfg.CACertFile, cfg.ResponderCertFile,
+		cfg.KeyFile, cfg.Interval)
 }
 
