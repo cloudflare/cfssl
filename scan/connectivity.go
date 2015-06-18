645f42b969cfee54b848e936eb85c62a68ed5059
Jacob H. Haven <jacob@cloudflare.com>
2015-06-18 16:40:39 -0700

    scan: add scan to check if host is orange-clouded

--- scan/connectivity.go
+++ scan/connectivity.go
@@ -2,8 +2,8 @@ package scan
 
 import (
 	"errors"
+	"log"
 	"net"
-	"strings"
 
 	"github.com/cloudflare/cf-tls/tls"
 )
@@ -16,6 +16,10 @@ var Connectivity = &Family{
 			"Host can be resolved through DNS",
 			dnsLookupScan,
 		},
+		"CloudFlareStatus": {
+			"Host is on CloudFlare",
+			onCloudFlareScan,
+		},
 		"TCPDial": {
 			"Host accepts TCP connection",
 			tcpDialScan,
@@ -27,11 +31,39 @@ var Connectivity = &Family{
 	},
 }
 
-// lookupAddrs is a list of host's addresses returned by DNS lookup
-type lookupAddrs []string
+var cloudflareIPs = [...]string{
+	"103.21.244.0/22",
+	"103.22.200.0/22",
+	"103.31.4.0/22",
+	"104.16.0.0/12",
+	"108.162.192.0/18",
+	"141.101.64.0/18",
+	"162.158.0.0/15",
+	"172.64.0.0/13",
+	"173.245.48.0/20",
+	"188.114.96.0/20",
+	"190.93.240.0/20",
+	"197.234.240.0/22",
+	"198.41.128.0/17",
+	"199.27.128.0/21",
 
-func (addrs lookupAddrs) String() string {
-	return strings.Join(addrs, "\n")
+	"2400:cb00::/32",
+	"2405:8100::/32",
+	"2405:b500::/32",
+	"2606:4700::/32",
+	"2803:f800::/32",
+}
+
+var cloudflareNets []*net.IPNet
+
+func init() {
+	for _, s := range cloudflareIPs {
+		_, ipnet, err := net.ParseCIDR(s)
+		if err != nil {
+			log.Fatalf("Couldn't parse CIDR range: %v", err)
+		}
+		cloudflareNets = append(cloudflareNets, ipnet)
+	}
 }
 
 // dnsLookupScan tests that DNS resolution of the host returns at least one address
@@ -41,8 +73,7 @@ func dnsLookupScan(host string) (grade Grade, output Output, err error) {
 		return
 	}
 
-	var addrs lookupAddrs
-	addrs, err = net.LookupHost(host)
+	addrs, err := net.LookupHost(host)
 	if err != nil {
 		return
 	}
@@ -50,7 +81,34 @@ func dnsLookupScan(host string) (grade Grade, output Output, err error) {
 	if len(addrs) == 0 {
 		err = errors.New("no addresses found for host")
 	}
+
 	grade, output = Good, addrs
+	return
+}
+
+func onCloudFlareScan(host string) (grade Grade, output Output, err error) {
+	_, addrs, err := dnsLookupScan(host)
+	if err != nil {
+		return
+	}
+
+	cfStatus := make(map[string]bool)
+	grade = Good
+	for _, addr := range addrs.([]string) {
+		ip := net.ParseIP(addr)
+		for _, cfNet := range cloudflareNets {
+			if cfNet.Contains(ip) {
+				cfStatus[addr] = true
+				break
+			}
+		}
+		if !cfStatus[addr] {
+			cfStatus[addr] = false
+			grade = Bad
+		}
+	}
+
+	output = cfStatus
 	return
 }
 
