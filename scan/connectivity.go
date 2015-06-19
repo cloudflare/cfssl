f6dae78e025b7077de69ec404de3ceeabfef9f88
Jacob H. Haven <jacob@cloudflare.com>
2015-06-18 17:27:53 -0700

    scan: download updated IP ranges at runtime instead of storing them statically

--- scan/connectivity.go
+++ scan/connectivity.go
@@ -1,9 +1,13 @@
 package scan
 
 import (
+	"bufio"
 	"errors"
+	"io"
 	"log"
 	"net"
+	"net/http"
+	"strings"
 
 	"github.com/cloudflare/cf-tls/tls"
 )
@@ -31,38 +35,32 @@ var Connectivity = &Family{
 	},
 }
 
-var cloudflareIPs = [...]string{
-	"103.21.244.0/22",
-	"103.22.200.0/22",
-	"103.31.4.0/22",
-	"104.16.0.0/12",
-	"108.162.192.0/18",
-	"141.101.64.0/18",
-	"162.158.0.0/15",
-	"172.64.0.0/13",
-	"173.245.48.0/20",
-	"188.114.96.0/20",
-	"190.93.240.0/20",
-	"197.234.240.0/22",
-	"198.41.128.0/17",
-	"199.27.128.0/21",
-
-	"2400:cb00::/32",
-	"2405:8100::/32",
-	"2405:b500::/32",
-	"2606:4700::/32",
-	"2803:f800::/32",
-}
-
 var cloudflareNets []*net.IPNet
 
 func init() {
-	for _, s := range cloudflareIPs {
-		_, ipnet, err := net.ParseCIDR(s)
+	// Download CloudFlare CIDR ranges and parse them.
+	v4resp, err := http.Get("https://www.cloudflare.com/ips-v4")
+	if err != nil {
+		log.Fatalf("Couldn't download CloudFlare IPs: %v", err)
+	}
+	defer v4resp.Body.Close()
+
+	v6resp, err := http.Get("https://www.cloudflare.com/ips-v6")
+	if err != nil {
+		log.Fatalf("Couldn't download CloudFlare IPs: %v", err)
+	}
+	defer v6resp.Body.Close()
+
+	scanner := bufio.NewScanner(io.MultiReader(v4resp.Body, strings.NewReader("\n"), v6resp.Body))
+	for scanner.Scan() {
+		_, ipnet, err := net.ParseCIDR(scanner.Text())
 		if err != nil {
 			log.Fatalf("Couldn't parse CIDR range: %v", err)
 		}
 		cloudflareNets = append(cloudflareNets, ipnet)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Couldn't read IP bodies: %v", err)
 	}
 }
 
