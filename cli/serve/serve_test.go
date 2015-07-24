06aa4eaf87925352917c898b0309e2070190c77c
Jacob H. Haven <jacob@cloudflare.com>
2015-07-24 16:58:13 -0700

    serve: Update serve_test to use go.rice

--- cli/serve/serve_test.go
+++ cli/serve/serve_test.go
@@ -3,7 +3,10 @@ package serve
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	rice "github.com/GeertJohan/go.rice"
 )
 
 func TestServe(t *testing.T) {
@@ -12,34 +15,48 @@ func TestServe(t *testing.T) {
 	defer ts.Close()
 	expected := make(map[string]int)
 	for endpoint := range v1Endpoints {
-		expected[endpoint] = http.StatusOK
+		expected[v1APIPath(endpoint)] = http.StatusOK
 	}
-	for staticEndpoint := range _escData {
-		expected[staticEndpoint] = http.StatusOK
+
+	staticDir := "static"
+	err := rice.MustFindBox(staticDir).Walk("", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			expected["/"+path] = http.StatusOK
+		}
+		return nil
+	})
+	if err != nil {
+		t.Error(err)
 	}
 
 	// Disabled endpoints should return '404 Not Found'
-	expected["sign"] = http.StatusNotFound
-	expected["authsign"] = http.StatusNotFound
-	expected["newcert"] = http.StatusNotFound
-	expected["info"] = http.StatusNotFound
-	expected["bundle"] = http.StatusNotFound
+	expected[v1APIPath("sign")] = http.StatusNotFound
+	expected[v1APIPath("authsign")] = http.StatusNotFound
+	expected[v1APIPath("newcert")] = http.StatusNotFound
+	expected[v1APIPath("info")] = http.StatusNotFound
+	expected[v1APIPath("bundle")] = http.StatusNotFound
 
 	// Enabled endpoints should return '405 Method Not Allowed'
-	expected["init_ca"] = http.StatusMethodNotAllowed
-	expected["newkey"] = http.StatusMethodNotAllowed
+	expected[v1APIPath("init_ca")] = http.StatusMethodNotAllowed
+	expected[v1APIPath("newkey")] = http.StatusMethodNotAllowed
 
 	// POST-only endpoints should return '400 Bad Request'
-	expected["scan"] = http.StatusBadRequest
+	expected[v1APIPath("scan")] = http.StatusBadRequest
 
 	// Non-existent endpoints should return '404 Not Found'
-	expected["bad_endpoint"] = http.StatusNotFound
 	expected["/bad_endpoint"] = http.StatusNotFound
 
 	for endpoint, status := range expected {
-		resp, _ := http.Get(ts.URL + v1APIPath(endpoint))
-		if resp.StatusCode != expected[endpoint] {
-			t.Fatalf("%s: '%s' (expected '%s')", v1APIPath(endpoint), resp.Status, http.StatusText(status))
+		resp, err := http.Get(ts.URL + endpoint)
+		if err != nil {
+			t.Error(err)
+		}
+		if resp.StatusCode != status {
+			t.Fatalf("%s: '%s' (expected '%s')", endpoint, resp.Status, http.StatusText(status))
 		}
 	}
 }
