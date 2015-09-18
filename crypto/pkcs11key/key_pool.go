e06d229aaa235df77e1c3dc42fc44678b3355fe6
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-09-18 15:58:33 -0700

    Rename PKCS11KeyPool to Pool.

--- crypto/pkcs11key/key_pool.go
+++ crypto/pkcs11key/key_pool.go
@@ -7,17 +7,17 @@ import (
 	"fmt"
 )
 
-// PKCS11KeyPool is a pool of PKCS11Keys suitable for high performance parallel
+// Pool is a pool of PKCS11Keys suitable for high performance parallel
 // work. PKCS11Key on its own is suitable for multi-threaded use because it has
 // built-in locking, but one PKCS11Key can have at most one operation inflight
 // at a time. If you are using an HSM that supports multiple sessions, you may
-// want to use a PKCS11KeyPool instead, which contains multiple signers.
-// PKCS11KeyPool satisfies the Signer interface just as PKCS11Key does, and
+// want to use a Pool instead, which contains multiple signers.
+// Pool satisfies the Signer interface just as PKCS11Key does, and
 // farms out work to multiple sessions under the hood. This assumes you are
 // calling Sign from multiple goroutines (as would be common in an RPC or HTTP
 // environment). If you only call Sign from a single goroutine, you will only
 // ever get single-session performance.
-type PKCS11KeyPool struct {
+type Pool struct {
 	// This slice acts more or less like a concurrent stack. Keys are popped off
 	// the top for use, and then pushed back on when they are no longer in use.
 	signers    []*PKCS11Key
@@ -28,7 +28,7 @@ type PKCS11KeyPool struct {
 	cond    *sync.Cond
 }
 
-func (p *PKCS11KeyPool) get() *PKCS11Key {
+func (p *Pool) get() *PKCS11Key {
 	p.cond.L.Lock()
 	for len(p.signers) == 0 {
 		p.cond.Wait()
@@ -40,7 +40,7 @@ func (p *PKCS11KeyPool) get() *PKCS11Key {
 	return instance
 }
 
-func (p *PKCS11KeyPool) put(instance *PKCS11Key) {
+func (p *Pool) put(instance *PKCS11Key) {
 	p.cond.L.Lock()
 	p.signers = append(p.signers, instance)
 	p.cond.Signal()
@@ -49,7 +49,7 @@ func (p *PKCS11KeyPool) put(instance *PKCS11Key) {
 
 // Sign performs a signature using an available PKCS #11 key. If there is no key
 // available, it blocks until there is.
-func (p *PKCS11KeyPool) Sign(rand io.Reader, msg []byte, opts crypto.SignerOpts) ([]byte, error) {
+func (p *Pool) Sign(rand io.Reader, msg []byte, opts crypto.SignerOpts) ([]byte, error) {
 	instance := p.get()
 	defer p.put(instance)
 	return instance.Sign(rand, msg, opts)
@@ -58,14 +58,14 @@ func (p *PKCS11KeyPool) Sign(rand io.Reader, msg []byte, opts crypto.SignerOpts)
 // Public returns the public key of any one of the signers in the pool. Since
 // they were all created with the same arguments, the public key should be the
 // same for each one.
-func (p *PKCS11KeyPool) Public() crypto.PublicKey {
+func (p *Pool) Public() crypto.PublicKey {
 	instance := p.get()
 	defer p.put(instance)
 	return instance.Public()
 }
 
 // NewPool creates a pool of PKCS11Keys of size n.
-func NewPool(n int, modulePath, tokenLabel, pin, privateKeyLabel string) (*PKCS11KeyPool, error) {
+func NewPool(n int, modulePath, tokenLabel, pin, privateKeyLabel string) (*Pool, error) {
 	var err error
 	signers := make([]*PKCS11Key, n)
 	for i := 0; i < n; i++ {
@@ -81,7 +81,7 @@ func NewPool(n int, modulePath, tokenLabel, pin, privateKeyLabel string) (*PKCS1
 	}
 
 	var mutex sync.Mutex
-	return &PKCS11KeyPool{
+	return &Pool{
 		signers: signers,
 		totalCount: len(signers),
 		cond: sync.NewCond(&mutex),
@@ -90,7 +90,7 @@ func NewPool(n int, modulePath, tokenLabel, pin, privateKeyLabel string) (*PKCS1
 
 // Destroy calls destroy for each of the member keys, shutting down their
 // sessions.
-func (p *PKCS11KeyPool) Destroy() {
+func (p *Pool) Destroy() {
 	for i := 0; i < p.totalCount; i++ {
 		p.get().Destroy()
 	}
