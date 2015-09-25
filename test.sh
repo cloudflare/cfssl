6ac4d5f79d2f27965b5fe28e0223be011f55d837
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-09-25 09:45:22 -0700

    Improve tests.
    Pull out a test.sh that includes golint and go vet.
    Don't `go get` cfssl: It should already be checked out as part of Travis' setup.
    This step was overwriting user-specific repos.

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+cd $(dirname $0)
+ls $GOPATH/src/github.com/cloudflare/cfssl
+
+go vet ./...
+if ! which fgt > /dev/null ; then
+  echo "Please install fgt from https://github.com/GeertJohan/fgt."
+  exit 1
+fi
+if ! which golint > /dev/null ; then
+  echo "Please install golint from github.com/golang/lint/golint."
+  exit 1
+fi
+fgt golint ./...
+go test ./...
+go list -f '{{if len .TestGoFiles}}"go test -coverprofile={{.Dir}}/.coverprofile {{.ImportPath}}"{{end}}' ./... | xargs -i sh -c {}
+gover . coverprofile.txt
