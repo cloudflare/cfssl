faa566b546a76fe77d1ace424a46fab7e101e212
Annie Phan <aphan1208@yahoo.com>
2015-10-09 12:24:49 -0700

    output test for log
    log test
    
    finished rest of log tests

--- log/log_test.go
+++ log/log_test.go
@@ -2,9 +2,7 @@ package log
 
 import (
 	"bytes"
-	"fmt"
 	"log"
-	"os"
 	"strings"
 	"testing"
 )
@@ -15,14 +13,175 @@ func TestOutputf(t *testing.T) {
 
 	log.SetOutput(buf)
 	outputf(LevelDebug, string1, nil)
-	line := buf.String()
 
-	//test with invalid inputs.
-	if strings.Contains(line, string1) {
-		return
-	} else {
+	// test 1: outputf correctly prints string
+	if !strings.Contains(buf.String(), string1) {
 		t.Fail()
 	}
 
+	(*bytes.Buffer).Reset(buf)
+	outputf(LevelDebug-1, string1, nil)
+
+	// test 2: outputf will not print if level is below a range
+	if buf.String() != "" {
+		t.Fail()
+	}
 	return
+}
+
+func TestOutput(t *testing.T) {
+	buf := new(bytes.Buffer)
+
+	log.SetOutput(buf)
+	output(LevelDebug, nil)
+
+	// test 1: outputf correctly prints string with proper Debug prefix
+	if !strings.Contains(buf.String(), levelPrefix[LevelDebug]) {
+		t.Fail()
+	}
+
+	(*bytes.Buffer).Reset(buf)
+	output(LevelDebug-1, nil)
+
+	// test 2: outputf will not print if level is below a range
+	if buf.String() != "" {
+		t.Fail()
+	}
+	return
+}
+
+func TestCriticalf(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Criticalf(string1, nil)
+
+	// test 1: outputf correctly prints string
+	// should never fail because critical > debug
+	if !strings.Contains(buf.String(), string1) {
+		t.Fail()
+	}
+	return
+}
+
+func TestCritical(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Critical(nil)
+
+	// test 1: outputf correctly prints string
+	if !strings.Contains(buf.String(), levelPrefix[LevelCritical]) {
+		t.Fail()
+	}
+	return
+}
+
+func TestErrorf(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Errorf(string1, nil)
+
+	// test 1: outputf correctly prints string
+	// should never fail because error > debug
+	if !strings.Contains(buf.String(), string1) {
+		t.Fail()
+	}
+	return
+}
+
+func TestError(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Error(nil)
+
+	// test 1: outputf correctly prints string
+	if !strings.Contains(buf.String(), levelPrefix[LevelError]) {
+		t.Fail()
+	}
+	return
+}
+
+func TestWarningf(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Warningf(string1, nil)
+
+	// test 1: outputf correctly prints string
+	// should never fail because fatal critical > debug
+	if !strings.Contains(buf.String(), string1) {
+		t.Fail()
+	}
+	return
+}
+
+func TestWarning(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Warning(nil)
+
+	// test 1: outputf correctly prints string
+	if !strings.Contains(buf.String(), levelPrefix[LevelWarning]) {
+		t.Fail()
+	}
+	return
+}
+
+func TestInfof(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Infof(string1, nil)
+
+	// test 1: outputf correctly prints string
+	// should never fail because fatal info > debug
+	if !strings.Contains(buf.String(), string1) {
+		t.Fail()
+	}
+	return
+}
+
+func TestInfo(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Info(nil)
+
+	// test 1: outputf correctly prints string
+	if !strings.Contains(buf.String(), levelPrefix[LevelInfo]) {
+		t.Fail()
+	}
+	return
+}
+
+func TestDebugf(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Debugf(string1, nil)
+
+	// test 1: outputf correctly prints string
+	// should never fail because fatal debug >= debug
+	if !strings.Contains(buf.String(), string1) {
+		t.Fail()
+	}
+	return
+}
+
+func TestDebug(t *testing.T) {
+	const string1 = "asdf123"
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	Debug(nil)
+
+	// test 1: outputf correctly prints string
+	if !strings.Contains(buf.String(), levelPrefix[LevelDebug]) {
+		t.Fail()
+	}
+	return
+
 }
