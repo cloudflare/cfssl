645674aeadfc1757c1c37620041d732b5fa6f480
Kyle Isom <kyle@cloudflare.com>
2015-10-27 13:57:48 -0700

    Clean up example server and client.
    * Move common functions to an example library.
    * Various clean ups: splitting things into separate functions and making
      it easier to extend the examples later.
    * Client sends a few messages this time, and the server will acknowledge
      them. In the future, a more extensive example might be useful.

--- transport/example/maserver/server.go
+++ transport/example/maserver/server.go
@@ -3,44 +3,18 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net"
-	"os"
-	"path/filepath"
-	"time"
 
+	"github.com/cloudflare/cfssl/log"
 	"github.com/cloudflare/cfssl/transport"
 	"github.com/cloudflare/cfssl/transport/core"
+	"github.com/cloudflare/cfssl/transport/example/exlib"
 )
 
 // maclient is a mutual-authentication server, meant to demonstrate
 // using the client-side mutual authentication side of the transport
 // package.
-
-var progname = filepath.Base(os.Args[0])
-var before = 5 * time.Minute
-
-// Err displays a formatting error message to standard error,
-// appending the error string, and exits with the status code from
-// `exit`, à la err(3).
-func Err(exit int, err error, format string, a ...interface{}) {
-	format = fmt.Sprintf("[%s] %s", progname, format)
-	format += ": %v\n"
-	a = append(a, err)
-	fmt.Fprintf(os.Stderr, format, a...)
-	os.Exit(exit)
-}
-
-// Warn displays a formatted error message to standard output,
-// appending the error string, à la warn(3).
-func Warn(err error, format string, a ...interface{}) (int, error) {
-	format = fmt.Sprintf("[%s] %s", progname, format)
-	format += ": %v\n"
-	a = append(a, err)
-	return fmt.Fprintf(os.Stderr, format, a...)
-}
 
 func main() {
 	var addr, conf string
@@ -51,29 +25,54 @@ func main() {
 	var id = new(core.Identity)
 	data, err := ioutil.ReadFile(conf)
 	if err != nil {
-		Err(1, err, "reading config file")
+		exlib.Err(1, err, "reading config file")
 	}
 
 	err = json.Unmarshal(data, id)
 	if err != nil {
-		Err(1, err, "parsing config file")
+		exlib.Err(1, err, "parsing config file")
 	}
 
-	tr, err := transport.New(before, id)
+	tr, err := transport.New(exlib.Before, id)
 	if err != nil {
-		Err(1, err, "creating transport")
+		exlib.Err(1, err, "creating transport")
 	}
 
 	l, err := transport.Listen(addr, tr)
 	if err != nil {
-		Err(1, err, "setting up listener")
+		exlib.Err(1, err, "setting up listener")
 	}
 
-	log.Println("setting up auto-update")
+	log.Info("setting up auto-update")
 	go l.AutoUpdate(nil, nil)
 
-	log.Println("listening on", addr)
-	Warn(serve(l), "serving listener")
+	log.Info("listening on ", addr)
+	exlib.Warn(serve(l), "serving listener")
+}
+
+func connHandler(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		buf, err := exlib.Unpack(conn)
+		if err != nil {
+			exlib.Warn(err, "unpack message")
+			return
+		}
+
+		if len(buf) == 0 {
+			log.Info(conn.RemoteAddr(), " sent empty record, closing connection")
+			return
+		}
+
+		log.Infof("received %d-byte message: %s", len(buf), buf)
+
+		err = exlib.Pack(conn, []byte("OK"))
+		if err != nil {
+			exlib.Warn(err, "pack message")
+			return
+		}
+	}
 }
 
 func serve(l net.Listener) error {
@@ -81,18 +80,11 @@ func serve(l net.Listener) error {
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			Warn(err, "client connection failed")
+			exlib.Warn(err, "client connection failed")
 			continue
 		}
-		go func(conn net.Conn) {
-			defer conn.Close()
-			buf, err := ioutil.ReadAll(conn)
-			if err != nil {
-				Warn(err, "reading from client")
-				return
-			}
 
-			log.Printf("received %d-byte message: %s", len(buf), buf)
-		}(conn)
+		log.Info("connection from ", conn.RemoteAddr())
+		go connHandler(conn)
 	}
 }
