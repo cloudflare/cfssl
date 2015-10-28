0dcd15e71ab290823f1189a4ab6631faa6a6474e
Mathias Brossard <mathias@brossard.org>
2015-10-27 22:20:37 -0700

    Adding unit test for ECDSA PKCS#11 key

--- crypto/pkcs11key/key_test.go
+++ crypto/pkcs11key/key_test.go
@@ -6,20 +6,22 @@ import (
 	"bytes"
 	"crypto"
 	"crypto/rand"
-	"testing"
-
 	"github.com/miekg/pkcs11"
+	"testing"
 )
 
 type mockCtx struct{}
 
-const privateKeyHandle = pkcs11.ObjectHandle(23)
+const rsaPrivateKeyHandle = pkcs11.ObjectHandle(23)
+const ecPrivateKeyHandle = pkcs11.ObjectHandle(32)
+const ecPublicKeyHandle = pkcs11.ObjectHandle(33)
 const sessionHandle = pkcs11.SessionHandle(17)
 
 var slots = []uint{7, 8, 9}
 var tokenInfo = pkcs11.TokenInfo{
 	Label: "token label",
 }
+var currentSearch []*pkcs11.Attribute
 
 func (c mockCtx) CloseSession(sh pkcs11.SessionHandle) error {
 	return nil
@@ -30,14 +32,29 @@ func (c mockCtx) FindObjectsFinal(sh pkcs11.SessionHandle) error {
 }
 
 func (c mockCtx) FindObjectsInit(sh pkcs11.SessionHandle, temp []*pkcs11.Attribute) error {
+	currentSearch = temp
 	return nil
 }
 
 func (c mockCtx) FindObjects(sh pkcs11.SessionHandle, max int) ([]pkcs11.ObjectHandle, bool, error) {
-	return []pkcs11.ObjectHandle{privateKeyHandle}, true, nil
+	for _, a := range currentSearch {
+		// We search private keys using CKA_LABEL
+		if a.Type == pkcs11.CKA_LABEL {
+			if a.Value[0] == 0x72 { // Label start with 'r'
+				return []pkcs11.ObjectHandle{rsaPrivateKeyHandle}, true, nil
+			} else if a.Value[0] == 0x65 { // Label start with 'e'
+				return []pkcs11.ObjectHandle{ecPrivateKeyHandle}, true, nil
+			}
+		}
+		// We search th EC public key using CKA_ID
+		if a.Type == pkcs11.CKA_ID {
+			return []pkcs11.ObjectHandle{ecPublicKeyHandle}, true, nil
+		}
+	}
+	return nil, false, nil
 }
 
-func (c mockCtx) GetAttributeValue(sh pkcs11.SessionHandle, o pkcs11.ObjectHandle, template []*pkcs11.Attribute) ([]*pkcs11.Attribute, error) {
+func rsaPrivateAttributes(template []*pkcs11.Attribute) ([]*pkcs11.Attribute, error) {
 	var output []*pkcs11.Attribute
 	for _, a := range template {
 		// Return simple values for these attributes. Note that a value of `1` for
@@ -51,8 +68,83 @@ func (c mockCtx) GetAttributeValue(sh pkcs11.SessionHandle, o pkcs11.ObjectHandl
 				Value: []byte{byte(1)},
 			})
 		}
+		if a.Type == pkcs11.CKA_KEY_TYPE {
+			output = append(output, &pkcs11.Attribute{
+				Type:  a.Type,
+				Value: []byte{byte(0)},
+			})
+		}
 	}
 	return output, nil
+}
+
+var ecOid = []byte{0x06, 0x08, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07}
+
+func ecPrivateAttributes(template []*pkcs11.Attribute) ([]*pkcs11.Attribute, error) {
+	var output []*pkcs11.Attribute
+	for _, a := range template {
+		if a.Type == pkcs11.CKA_EC_PARAMS {
+			output = append(output, &pkcs11.Attribute{
+				Type:  a.Type,
+				Value: ecOid,
+			})
+		}
+		if a.Type == pkcs11.CKA_KEY_TYPE ||
+			a.Type == pkcs11.CKA_ID {
+			output = append(output, &pkcs11.Attribute{
+				Type:  a.Type,
+				Value: []byte{byte(3)},
+			})
+		}
+	}
+	return output, nil
+}
+
+var ecPoint = []byte{0x04, 0x41, 0x04, 0x4C, 0xD7, 0x7B, 0x7B, 0x2E,
+	0x3D, 0x57, 0x98, 0xB8, 0x2F, 0x99, 0xB4, 0x83,
+	0x99, 0xE6, 0xD4, 0x4C, 0x4F, 0xBC, 0x2D, 0x60,
+	0xCD, 0x08, 0x8E, 0x93, 0x65, 0x6F, 0x20, 0x51,
+	0x1C, 0xE7, 0xFD, 0x59, 0x34, 0xAA, 0xA9, 0x36,
+	0x26, 0xCE, 0x4A, 0xC5, 0xA2, 0x4A, 0x85, 0x6C,
+	0xB3, 0x95, 0xFF, 0x92, 0x0F, 0x56, 0x76, 0x34,
+	0x1F, 0x69, 0x52, 0x5F, 0x20, 0x83, 0x13, 0x50,
+	0xA3, 0xDE, 0xBE}
+
+func ecPublicAttributes(template []*pkcs11.Attribute) ([]*pkcs11.Attribute, error) {
+	var output []*pkcs11.Attribute
+	for _, a := range template {
+		if a.Type == pkcs11.CKA_EC_PARAMS {
+			output = append(output, &pkcs11.Attribute{
+				Type:  a.Type,
+				Value: ecOid,
+			})
+		}
+		if a.Type == pkcs11.CKA_EC_POINT {
+			output = append(output, &pkcs11.Attribute{
+				Type:  a.Type,
+				Value: ecPoint,
+			})
+		}
+		if a.Type == pkcs11.CKA_KEY_TYPE ||
+			a.Type == pkcs11.CKA_ID {
+			output = append(output, &pkcs11.Attribute{
+				Type:  a.Type,
+				Value: []byte{byte(3)},
+			})
+		}
+	}
+	return output, nil
+}
+
+func (c mockCtx) GetAttributeValue(sh pkcs11.SessionHandle, o pkcs11.ObjectHandle, template []*pkcs11.Attribute) ([]*pkcs11.Attribute, error) {
+	if o == rsaPrivateKeyHandle {
+		return rsaPrivateAttributes(template)
+	} else if o == ecPrivateKeyHandle {
+		return ecPrivateAttributes(template)
+	} else if o == ecPublicKeyHandle {
+		return ecPublicAttributes(template)
+	}
+	return nil, nil
 }
 
 func (c mockCtx) GetSlotList(tokenPresent bool) ([]uint, error) {
@@ -93,27 +185,26 @@ func TestSetup(t *testing.T) {
 		tokenLabel: "token label",
 		pin:        "unused",
 	}
-	err := ps.setup("private key label")
+	err := ps.setup("rsa")
 	if err != nil {
 		t.Errorf("Failed to set up Key: %s", err)
 	}
 }
 
-func setup(t *testing.T) *Key {
+func setup(t *testing.T, label string) *Key {
 	ps := Key{
 		module:     mockCtx{},
 		tokenLabel: "token label",
 		pin:        "unused",
 	}
-	err := ps.setup("private key label")
+	err := ps.setup(label)
 	if err != nil {
 		t.Fatalf("Failed to set up Key: %s", err)
 	}
 	return &ps
 }
 
-func TestSign(t *testing.T) {
-	ps := setup(t)
+func sign(t *testing.T, ps *Key) {
 	// Sign input must be exactly 32 bytes to match SHA256 size. In normally
 	// usage, Sign would be called by e.g. x509.CreateCertificate, which would
 	// handle padding to the necessary size.
@@ -125,6 +216,14 @@ func TestSign(t *testing.T) {
 	if !bytes.Equal(output, []byte("some signed data")) {
 		t.Fatal("Incorrect sign output")
 	}
+}
+
+func TestSign(t *testing.T) {
+	ps := setup(t, "rsa")
+	sign(t, ps)
+
+	ps = setup(t, "ec")
+	sign(t, ps)
 }
 
 // This is a version of the mock that gives CKR_ATTRIBUTE_TYPE_INVALID when
@@ -148,7 +247,7 @@ func TestAttributeTypeInvalid(t *testing.T) {
 		tokenLabel: "token label",
 		pin:        "unused",
 	}
-	err := ps.setup("private key label")
+	err := ps.setup("rsa")
 	if err != nil {
 		t.Errorf("Failed to set up with a token that returns CKR_ATTRIBUTE_TYPE_INVALID: %s", err)
 	}
