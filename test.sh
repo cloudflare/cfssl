d3721ea035879739aa72116da5c537d1cb74a000
Kyle Isom <kyle@cloudflare.com>
2015-12-04 13:48:21 -0800

    Fix code coverage report (closes #437).

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -o errexit
+
 cd $(dirname $0)
 ls $GOPATH/src/github.com/cloudflare/cfssl
 
@@ -7,23 +8,29 @@ PACKAGES=$(go list ./... | grep -v /vendor/)
 
 go vet $PACKAGES
 if ! which fgt > /dev/null ; then
-  echo "Please install fgt from https://github.com/GeertJohan/fgt."
-  exit 1
+    echo "Please install fgt from https://github.com/GeertJohan/fgt."
+    exit 1
 fi
 
 if ! which golint > /dev/null ; then
-  echo "Please install golint from github.com/golang/lint/golint."
-  exit 1
+    echo "Please install golint from github.com/golang/lint/golint."
+    exit 1
 fi
 
 for package in $PACKAGES
 do
-        fgt golint ${package##github.com/cloudflare/cfssl/}/*.go
+    fgt golint ${package##github.com/cloudflare/cfssl/}/*.go
 done
 
 # Build and install cfssl executable in PATH
 go install github.com/cloudflare/cfssl/cmd/cfssl
-
 go test $BUILD_FLAGS $PACKAGES
-go list -f '{{if len .TestGoFiles}}"go test -coverprofile={{.Dir}}/.coverprofile {{.ImportPath}}"{{end}}' $PACKAGES | xargs -I "sh -c {}"
-gover . coverprofile.txt
+
+COVPROFILES=""
+for package in $(go list -f '{{if len .TestGoFiles}}{{.ImportPath}}{{end}}' $PACKAGES)
+do
+    profile="$GOPATH/src/$package/.coverprofile"
+    go test --coverprofile=$profile $package
+    [ -s $profile ] && COVPROFILES="$COVPROFILES $profile"
+done
+cat $COVPROFILES > coverprofile.txt
