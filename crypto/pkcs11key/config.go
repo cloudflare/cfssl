ce1d60cc54ff11695366a74e5a4ef4b22eae7824
Jeremy Wagner-Kaiser <jwagnerk@cloudflare.com>
2016-02-11 14:48:41 -0800

    Hoise PKCS11 config into OCSP config to break PKCS11 depdency.

--- crypto/pkcs11key/config.go
+++ crypto/pkcs11key/config.go
@@ -1,10 +1,11 @@
 package pkcs11key
 
+import (
+	OcspConfig "github.com/cloudflare/cfssl/ocsp/config"
+)
+
 // Config contains configuration information required to use a PKCS
 // #11 key.
 type Config struct {
-	Module          string
-	TokenLabel      string
-	PIN             string
-	PrivateKeyLabel string
+	OcspConfig.PKCS11Config
 }
--- ocsp/config/config.go
+++ ocsp/config/config.go
@@ -2,18 +2,24 @@
 // signer.
 package config
 
-import (
-	"time"
-	"github.com/cloudflare/cfssl/crypto/pkcs11key"
-)
+import "time"
+
+// PKCS11Config contains configuration information required to use a PKCS
+// #11 key.
+type PKCS11Config struct {
+	Module          string
+	TokenLabel      string
+	PIN             string
+	PrivateKeyLabel string
+}
 
 // Config contains configuration information required to set up an OCSP
 // signer. If PKCS11.Module is non-empty, PKCS11 signing will be used.
 // Otherwise signing from a key file will be used.
 type Config struct {
-	CACertFile string
+	CACertFile        string
 	ResponderCertFile string
-	KeyFile string
-	Interval time.Duration
-	PKCS11 pkcs11key.Config
+	KeyFile           string
+	Interval          time.Duration
+	PKCS11            PKCS11Config
 }
