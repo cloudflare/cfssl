da4b998bc04f8797188a54eeec2b0043aae8503d
Zi Lin <zi@cloudflare.com>
2016-02-14 21:22:23 -0800

    merge sqlite 001/002 migration scripts

--- certdb/sqlite/migrations/001_CreateCertificates.sql
+++ certdb/sqlite/migrations/001_CreateCertificates.sql
@@ -13,8 +13,18 @@ CREATE TABLE certificates (
   PRIMARY KEY(serial_number, authority_key_identifier)
 );
 
+CREATE TABLE ocsp_responses (
+  serial_number            bytea NOT NULL,
+  authority_key_identifier bytea NOT NULL,
+  body                     bytea NOT NULL,
+  expiry                   timestamp,
+  PRIMARY KEY(serial_number, authority_key_identifier),
+  FOREIGN KEY(serial_number) REFERENCES certificates(serial_number)
+);
+
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
+DROP TABLE ocsp_responses;
 DROP TABLE certificates;
 
