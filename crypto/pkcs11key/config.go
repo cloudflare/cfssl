8a6c92242886e57e979fe1cde1b38db46f7d654f
Jeremy Wagner-Kaiser <jwagnerk@cloudflare.com>
2016-02-16 13:59:02 -0800

    pkcs11key is now fully self-contained

--- crypto/pkcs11key/config.go
+++ crypto/pkcs11key/config.go
@@ -1,9 +1,10 @@
 package pkcs11key
 
-import (
-	ocsp "github.com/cloudflare/cfssl/ocsp/config"
-)
-
 // Config contains configuration information required to use a PKCS
 // #11 key.
-type Config ocsp.PKCS11Config
+type Config struct {
+	Module          string
+	TokenLabel      string
+	PIN             string
+	PrivateKeyLabel string
+}
--- ocsp/config/config.go
+++ ocsp/config/config.go
@@ -4,15 +4,6 @@ package config
 
 import "time"
 
-// PKCS11Config contains configuration information required to use a PKCS
-// #11 key.
-type PKCS11Config struct {
-	Module          string
-	TokenLabel      string
-	PIN             string
-	PrivateKeyLabel string
-}
-
 // Config contains configuration information required to set up an OCSP
 // signer. If PKCS11.Module is non-empty, PKCS11 signing will be used.
 // Otherwise signing from a key file will be used.
@@ -21,5 +12,4 @@ type Config struct {
 	ResponderCertFile string
 	KeyFile           string
 	Interval          time.Duration
-	PKCS11            PKCS11Config
 }
