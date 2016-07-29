e6891b8869598a9957237cec0a8569aac60e08a1
Craine Runton <craine.runton@distilnetworks.com>
2016-07-29 10:48:41 -0600

    Remove SQLite dependency from cmd/cfssl/cfssl.go because it breaks the build on a number of archtectures

--- cmd/cfssl/cfssl.go
+++ cmd/cfssl/cfssl.go
@@ -45,7 +45,6 @@ import (
 
 	_ "github.com/go-sql-driver/mysql" // import to support MySQL
 	_ "github.com/lib/pq"              // import to support Postgres
-	_ "github.com/mattn/go-sqlite3"    // import to support SQLite3
 )
 
 // main defines the cfssl usage and registers all defined commands and flags.
