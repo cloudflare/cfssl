e77935af9a561574c76a1979bc875e7fb6efa29a
Dario Freddi <dario.freddi@ispirata.com>
2016-12-28 01:13:55 +0100

    certdb: Add a function for retrieving revoked certificates.
    This is going to be useful for building CRLs

--- certdb/certdb.go
+++ certdb/certdb.go
@@ -31,6 +31,7 @@ type Accessor interface {
 	InsertCertificate(cr CertificateRecord) error
 	GetCertificate(serial, aki string) ([]CertificateRecord, error)
 	GetUnexpiredCertificates() ([]CertificateRecord, error)
+	GetRevokedAndUnexpiredCertificates() ([]CertificateRecord, error)
 	RevokeCertificate(serial, aki string, reasonCode int) error
 	InsertOCSP(rr OCSPRecord) error
 	GetOCSP(serial, aki string) ([]OCSPRecord, error)
