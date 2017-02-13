1060657ee7e5e58fb8cbc51bd92e1e032798e35d
David Li <li.davidm96@gmail.com>
2017-02-13 13:16:11 -0500

    Optionally generate OCSP response in cert revocation API endpoint (#727)
    * Optionally generate OCSP response in cert revocation API endpoint
    
    * Make the signer used by api/revoke public
    
    * Use ParseCertificatePEM helper to simplify parsing
    
    * Style: move OCSPRecord construction outside the if statement
    
    * Add test of OCSP response generation after certificate revocation

--- api/revoke/revoke.go
+++ api/revoke/revoke.go
@@ -5,17 +5,22 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/cloudflare/cfssl/api"
 	"github.com/cloudflare/cfssl/certdb"
 	"github.com/cloudflare/cfssl/errors"
+	"github.com/cloudflare/cfssl/helpers"
 	"github.com/cloudflare/cfssl/ocsp"
+
+	stdocsp "golang.org/x/crypto/ocsp"
 )
 
 // A Handler accepts requests with a serial number parameter
 // and revokes
 type Handler struct {
 	dbAccessor certdb.Accessor
+	Signer     ocsp.Signer
 }
 
 // NewHandler returns a new http.Handler that handles a revoke request.
@@ -23,6 +28,18 @@ func NewHandler(dbAccessor certdb.Accessor) http.Handler {
 	return &api.HTTPHandler{
 		Handler: &Handler{
 			dbAccessor: dbAccessor,
+		},
+		Methods: []string{"POST"},
+	}
+}
+
+// NewOCSPHandler returns a new http.Handler that handles a revoke
+// request and also generates an OCSP response
+func NewOCSPHandler(dbAccessor certdb.Accessor, signer ocsp.Signer) http.Handler {
+	return &api.HTTPHandler{
+		Handler: &Handler{
+			dbAccessor: dbAccessor,
+			Signer:     signer,
 		},
 		Methods: []string{"POST"},
 	}
@@ -64,6 +81,55 @@ func (h *Handler) Handle(w http.ResponseWriter, r *http.Request) error {
 	err = h.dbAccessor.RevokeCertificate(req.Serial, req.AKI, reasonCode)
 	if err != nil {
 		return err
+	}
+
+	// If we were given a signer, try and generate an OCSP
+	// response indicating revocation
+	if h.Signer != nil {
+		// TODO: should these errors be errors?
+		// Grab the certificate from the database
+		cr, err := h.dbAccessor.GetCertificate(req.Serial, req.AKI)
+		if err != nil {
+			return err
+		}
+		if len(cr) != 1 {
+			return errors.NewBadRequestString("No unique certificate found")
+		}
+
+		cert, err := helpers.ParseCertificatePEM([]byte(cr[0].PEM))
+		if err != nil {
+			return errors.NewBadRequestString("Unable to parse certificates from PEM data")
+		}
+
+		sr := ocsp.SignRequest{
+			Certificate: cert,
+			Status:      "revoked",
+			Reason:      reasonCode,
+			RevokedAt:   time.Now().UTC(),
+		}
+
+		ocspResponse, err := h.Signer.Sign(sr)
+		if err != nil {
+			return err
+		}
+
+		// We parse the OCSP repsonse in order to get the next
+		// update time/expiry time
+		ocspParsed, err := stdocsp.ParseResponse(ocspResponse, nil)
+		if err != nil {
+			return err
+		}
+
+		ocspRecord := certdb.OCSPRecord{
+			Serial: req.Serial,
+			AKI:    req.AKI,
+			Body:   string(ocspResponse),
+			Expiry: ocspParsed.NextUpdate,
+		}
+
+		if err = h.dbAccessor.InsertOCSP(ocspRecord); err != nil {
+			return err
+		}
 	}
 
 	result := map[string]string{}
