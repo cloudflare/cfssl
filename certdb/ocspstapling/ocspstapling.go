cdce0aeab5bddce7420e57133edd8b0b5360fd26
Seth <saf258@cornell.edu>
2017-03-16 14:54:31 -0400

    Make comments into full sentences and remove unnecessary code

--- certdb/ocspstapling/ocspstapling.go
+++ certdb/ocspstapling/ocspstapling.go
@@ -9,7 +9,6 @@ import (
 	"encoding/asn1"
 	"encoding/base64"
 	"errors"
-	"fmt"
 	"github.com/cloudflare/cfssl/certdb"
 	cferr "github.com/cloudflare/cfssl/errors"
 	"github.com/cloudflare/cfssl/helpers"
@@ -25,19 +24,18 @@ var sctExtOid = asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 11129, 2, 4, 2}
 //
 // NOTE: This function is patterned after the exported Sign method in
 // https://github.com/cloudflare/cfssl/blob/master/signer/local/local.go
-func StapleSCTList(acc certdb.Accessor, serial, aki string, scts []ct.SignedCertificateTimestamp, issuer *x509.Certificate, priv crypto.Signer) error {
-	// Grab all OCSP records that match serial and aki
+func StapleSCTList(acc certdb.Accessor, serial, aki string, scts []ct.SignedCertificateTimestamp,
+	issuer *x509.Certificate, priv crypto.Signer) error {
 	ocspRecs, err := acc.GetOCSP(serial, aki)
 	if err != nil {
 		return err
 	}
 
 	if len(ocspRecs) == 0 {
-		// { OCSPRecord does not exist }
 		return cferr.Wrap(cferr.CertStoreError, cferr.RecordNotFound, errors.New("empty OCSPRecord"))
 	}
 
-	// Add the SCTs to each ocsp response
+	// This loop adds the SCTs to each OCSP response in ocspRecs.
 	for _, rec := range ocspRecs {
 		der, err := base64.StdEncoding.DecodeString(rec.Body)
 		if err != nil {
@@ -51,7 +49,6 @@ func StapleSCTList(acc certdb.Accessor, serial, aki string, scts []ct.SignedCert
 				errors.New("failed to parse DER-encoded OCSP response"))
 		}
 
-		// Serialize the list of SCTs
 		serializedSCTList, err := helpers.SerializeSCTList(scts)
 		if err != nil {
 			return cferr.Wrap(cferr.CTError, cferr.Unknown,
@@ -64,14 +61,13 @@ func StapleSCTList(acc certdb.Accessor, serial, aki string, scts []ct.SignedCert
 				errors.New("failed to serialize SCT list"))
 		}
 
-		// Add the SCT list to a new extension
 		sctExtension := pkix.Extension{
 			Id:       sctExtOid,
 			Critical: false,
 			Value:    serializedSCTList,
 		}
 
-		// Find the SCTListExtension in the ocsp response
+		// This loop finds the SCTListExtension in the ocsp response.
 		var idxExt int
 		for _, ext := range response.Extensions {
 			if ext.Id.Equal(sctExtOid) {
@@ -82,13 +78,12 @@ func StapleSCTList(acc certdb.Accessor, serial, aki string, scts []ct.SignedCert
 
 		newExtensions := response.Extensions
 		if idxExt >= len(response.Extensions) {
-			// { there's no SCT extension in response.Extensions }
+			// No SCT extension was found.
 			newExtensions = append(newExtensions, sctExtension)
 		}
 
-		// Write updated extensions to replace existing extensions in
-		// response when re-marshalling
-		fmt.Println(response.Certificate) // TODO: DELETE ME
+		// Here we write the updated extensions to replace the old
+		// response extensions when re-marshalling.
 		template := ocsp.Response{
 			Status:          response.Status,
 			SerialNumber:    response.Certificate.SerialNumber,
@@ -99,14 +94,13 @@ func StapleSCTList(acc certdb.Accessor, serial, aki string, scts []ct.SignedCert
 			IssuerHash:      response.IssuerHash,
 		}
 
-		// Re-sign response to generate the new DER-encoded response
+		// Finally, we re-sign the response to generate the new
+		// DER-encoded response.
 		der, err = ocsp.CreateResponse(issuer, response.Certificate, template, priv)
-
 		if err != nil {
 			return cferr.Wrap(cferr.CTError, cferr.Unknown,
 				errors.New("failed to sign new OCSP response"))
 		}
-
 		body := base64.StdEncoding.EncodeToString(der)
 		err = acc.UpdateOCSP(serial, aki, body, rec.Expiry)
 
