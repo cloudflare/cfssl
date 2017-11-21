2421a4ab31547682d159f14e4e2865fde6cda224
jlanger <jo.langer@gmail.com>
2017-11-21 17:07:40 +0100

    Added option to provide db config to ocsp serve command

--- cli/ocspserve/ocspserve.go
+++ cli/ocspserve/ocspserve.go
@@ -12,32 +12,42 @@ import (
 )
 
 // Usage text of 'cfssl serve'
-var ocspServerUsageText = `cfssl ocspserve -- set up an HTTP server that handles OCSP requests from a file (see RFC 5019)
+var ocspServerUsageText = `cfssl ocspserve -- set up an HTTP server that handles OCSP requests from either a file or directly from a database (see RFC 5019)
 
   Usage of ocspserve:
-          cfssl ocspserve [-address address] [-port port] [-responses file]
+          cfssl ocspserve [-address address] [-port port] [-responses file] [-db-config db-config]
 
   Flags:
   `
 
 // Flags used by 'cfssl serve'
-var ocspServerFlags = []string{"address", "port", "responses"}
+var ocspServerFlags = []string{"address", "port", "responses", "db-config"}
 
 // ocspServerMain is the command line entry point to the OCSP responder.
 // It sets up a new HTTP server that responds to OCSP requests.
 func ocspServerMain(args []string, c cli.Config) error {
+	var src ocsp.Source
 	// serve doesn't support arguments.
 	if len(args) > 0 {
 		return errors.New("argument is provided but not defined; please refer to the usage by flag -h")
 	}
 
-	if c.Responses == "" {
-		return errors.New("no response file provided, please set the -responses flag")
-	}
-
-	src, err := ocsp.NewSourceFromFile(c.Responses)
-	if err != nil {
-		return errors.New("unable to read response file")
+	if c.Responses != "" {
+		s, err := ocsp.NewSourceFromFile(c.Responses)
+		if err != nil {
+			return errors.New("unable to read response file")
+		}
+		src = s
+	} else if c.DBConfigFile != "" {
+		s, err := ocsp.NewSourceFromDB(c.DBConfigFile)
+		if err != nil {
+			return errors.New("unable to read configuration file")
+		}
+		src = s
+	} else {
+		return errors.New(
+			"no response file or db-config provided, please set the one of these using either -responses or -db-config flags",
+		)
 	}
 
 	log.Info("Registering OCSP responder handler")
