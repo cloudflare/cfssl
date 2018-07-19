43dc320c0260e6558daa80e0ebb3bf0642f78dff
Christopher Broglie <cbroglie@cloudflare.com>
2018-07-18 22:19:34 -0700

    Fix golint checks

--- api/health/health.go
+++ api/health/health.go
@@ -7,15 +7,17 @@ import (
 	"github.com/cloudflare/cfssl/api"
 )
 
-type HealthResponse struct {
+// Response contains the response to the /health API
+type Response struct {
 	Healthy bool `json:"healthy"`
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) error {
-	response := api.NewSuccessResponse(&HealthResponse{Healthy: true})
+	response := api.NewSuccessResponse(&Response{Healthy: true})
 	return json.NewEncoder(w).Encode(response)
 }
 
+// NewHealthCheck creates a new handler to serve health checks.
 func NewHealthCheck() http.Handler {
 	return api.HTTPHandler{
 		Handler: api.HandlerFunc(healthHandler),
