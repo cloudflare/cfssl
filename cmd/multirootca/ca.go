7c2aa5370604154f4fda0407ad151fa7de9beba9
andrewheberle <andrewheberle@users.noreply.github.com>
2019-01-29 09:55:41 +0800

    Add missing sqlite import
    Ensure that go-sqlite3 is imported so sqlite based databases work properly

--- cmd/multirootca/ca.go
+++ cmd/multirootca/ca.go
@@ -18,6 +18,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql" // import to support MySQL
 	_ "github.com/lib/pq"              // import to support Postgres
+	_ "github.com/mattn/go-sqlite3"    // import to support SQLite
 )
 
 func parseSigner(root *config.Root) (signer.Signer, error) {
