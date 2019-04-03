420d683f8a54be23d20f43fdfbdbc54dde235e56
Geoff Baskwill <me@geoffbaskwill.ca>
2019-04-03 16:04:20 -0400

    Add support for Subject Alternative Names in selfsign
    This adds support for `DNSNames`, `EmailAddresses`, `IPAddresses`,
    and `URIs` in the SAN parsed from the `hosts` attribute of the CSR
    JSON.
    
    Signed-off-by: Geoff Baskwill <me@geoffbaskwill.ca>

--- selfsign/selfsign_test.go
+++ selfsign/selfsign_test.go
@@ -1,7 +1,12 @@
 package selfsign
 
 import (
+	"crypto/x509"
+	"encoding/pem"
 	"io/ioutil"
+	"net"
+	"net/url"
+	"reflect"
 	"testing"
 	"time"
 
@@ -12,6 +17,8 @@ import (
 const (
 	keyFile = "testdata/localhost.key"
 	csrFile = "testdata/localhost.csr"
+
+	csr2File = "testdata/sans.csr"
 )
 
 func TestDefaultSign(t *testing.T) {
@@ -36,4 +43,60 @@ func TestDefaultSign(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestSANs(t *testing.T) {
+	csrBytes, err := ioutil.ReadFile(csr2File)
+	if err != nil {
+		t.Fatal(err)
+	}
+	keyBytes, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	priv, err := helpers.ParsePrivateKeyPEM(keyBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	profile := config.DefaultConfig()
+	profile.Expiry = 10 * time.Hour
+
+	certPEM, err := Sign(priv, csrBytes, profile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p, _ := pem.Decode(certPEM)
+	if p == nil || p.Type != "CERTIFICATE" {
+		// this seems unlikely
+		t.Fatalf("failed creating certificate")
+	}
+
+	cert, err := x509.ParseCertificate(p.Bytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedEmailAddresses := []string{"jdoe@example.com"}
+	if !reflect.DeepEqual(cert.EmailAddresses, expectedEmailAddresses) {
+		t.Errorf("cert should have contained EmailAddresses %#v but had %#v", expectedEmailAddresses, cert.EmailAddresses)
+	}
+
+	expectedDNSNames := []string{"cloudflare.com", "www.cloudflare.com"}
+	if !reflect.DeepEqual(cert.DNSNames, expectedDNSNames) {
+		t.Errorf("cert should have contained DNSNames %#v but had %#v", expectedDNSNames, cert.DNSNames)
+	}
+
+	expectedIPAddresses := []net.IP{net.IP{0xc0, 0xa8, 0x0, 0x1}}
+	if !reflect.DeepEqual(cert.IPAddresses, expectedIPAddresses) {
+		t.Errorf("cert should have contained IPAddresses %#v but had %#v", expectedIPAddresses, cert.IPAddresses)
+	}
+
+	expectedURIs := []*url.URL{&url.URL{Scheme: "https", Host: "www.cloudflare.com"}}
+	if !reflect.DeepEqual(cert.URIs, expectedURIs) {
+		t.Errorf("cert should have contained URIs %#v but had %#v", expectedURIs, cert.URIs)
+	}
+
 }
