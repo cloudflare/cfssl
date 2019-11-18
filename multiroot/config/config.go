cloudflare/cfssl

commit 726abc3ab84b5098dc47e9770b1fad9cd6a9537e

HSM support and debug logging added in mulirootca
With this mod applied, one can use PKCS #11 token in multirootca
for certs signing (tested succesfully with Nitrokey HSM 2; should
work with other PKCS #11 compatible HSM tokens too). Requires importing

    https://github.com/letsencrypt/pkcs11key

package (not included in this commit; please consider including
upstream).

Example config:

    [ default ]
    private = pkcs11://notused
    module = /usr/lib/x86_64-linux-gnu/opensc-pkcs11.so
    token_label = UserPIN (myhsm)
    pin = 123456
    config = /etc/cfssl/server.json
    certificate = /etc/cfssl/issuer.crt

Note: issuer.crt must contain cert issued for signing key in HSM.

This mod also adds loglevel parameter to multirootca command
to enable easier multirootca execution debug.

Author-Change-Id: IB#1094763

multiroot/config/config.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"crypto"
 	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -25,6 +26,7 @@ import (
 	"github.com/cloudflare/redoctober/client"
 	"github.com/cloudflare/redoctober/core"
 	"github.com/jmoiron/sqlx"
+	"github.com/letsencrypt/pkcs11key"
 )
 
 // RawMap is shorthand for the type used as a map from string to raw Root struct.
@@ -254,6 +256,47 @@ func parsePrivateKeySpec(spec string, cfg map[string]string) (crypto.Signer, err
 		}
 
 		log.Debug("loaded private key")
+
+		return priv, nil
+	case "pkcs11":
+		module := cfg["module"]
+		if module == "" {
+			return nil, errors.New("config: PKCS11 module path must be set (module)")
+		}
+
+		tokenLabel := cfg["token_label"]
+		if tokenLabel == "" {
+			return nil, errors.New("config: PKCS11 token label must be set (token_label)")
+		}
+
+		pin := cfg["pin"]
+		if pin == "" {
+			return nil, errors.New("config: PKCS11 PIN must be set (pin)")
+		}
+
+		certificate := cfg["certificate"]
+		if certificate == "" {
+			return nil, errors.New("config: PKCS11 public key path must be set (certificate)")
+		}
+
+		// load public key from certificate
+		p, err := ioutil.ReadFile(certificate)
+		if err != nil {
+			return nil, err
+		}
+		b, _ := pem.Decode(p)
+		cert, err := x509.ParseCertificate(b.Bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		// initialize token communication
+		priv, err = pkcs11key.New(module, tokenLabel, pin, cert.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+
+		log.Debug(fmt.Sprintf("pkcs11 communication with token %s initialized successfully", tokenLabel))
 
 		return priv, nil
 	default:

cmd/multirootca/ca.go
@@ -8,6 +8,8 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/letsencrypt/pkcs11key"
+
 	"github.com/cloudflare/cfssl/api/info"
 	"github.com/cloudflare/cfssl/certdb/sql"
 	"github.com/cloudflare/cfssl/log"
@@ -23,7 +25,7 @@ import (
 func parseSigner(root *config.Root) (signer.Signer, error) {
 	privateKey := root.PrivateKey
 	switch priv := privateKey.(type) {
-	case *rsa.PrivateKey, *ecdsa.PrivateKey:
+	case *rsa.PrivateKey, *ecdsa.PrivateKey, *pkcs11key.Key:
 		s, err := local.NewSigner(priv, root.Certificate, signer.DefaultSigAlgo(priv), nil)
 		if err != nil {
 			return nil, err
@@ -46,12 +48,18 @@ var (
 )
 
 func main() {
+	flagLogLevel := flag.Int("loglevel", log.LevelInfo, "Log level (0 = DEBUG, 5 = FATAL)")
 	flagAddr := flag.String("a", ":8888", "listening address")
 	flagRootFile := flag.String("roots", "", "configuration file specifying root keys")
 	flagDefaultLabel := flag.String("l", "", "specify a default label")
 	flagEndpointCert := flag.String("tls-cert", "", "server certificate")
 	flagEndpointKey := flag.String("tls-key", "", "server private key")
+
 	flag.Parse()
+
+	if *flagLogLevel >= 0 && *flagLogLevel <= 5 {
+		log.Level = *flagLogLevel
+	}
 
 	if *flagRootFile == "" {
 		log.Fatal("no root file specified")

commit 897b2127b4ffe97d8a7c8761f29ebc18f5665db1

Empty PIN allowed
Empty PIN may be configured to allow one to enter it on PIN pad
if available.

Fixes: 726abc3ab84b5098dc47e9770b1fad9cd6a9537e
Author-Change-Id: IB#1094763

multiroot/config/config.go
@@ -271,7 +271,7 @@ func parsePrivateKeySpec(spec string, cfg map[string]string) (crypto.Signer, err
 
 		pin := cfg["pin"]
 		if pin == "" {
-			return nil, errors.New("config: PKCS11 PIN must be set (pin)")
+            log.Infof("PKCS11 PIN for token %s is not specified in config; please enter it on PIN pad if available", tokenLabel)
 		}
 
 		certificate := cfg["certificate"]