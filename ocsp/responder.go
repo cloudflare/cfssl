cloudflare/cfssl

commit 5a716b1360abb09b245572fa477caf902185dfc6

Fixed handling empty responses from redis
Fixed panics when OCSP response is was not found in redis.

Fixes: 29ac288bf84806fa4a4c756cfea4e4feff65200f
Author-Change-Id: IB#1095303

ocsp/responder.go
@@ -116,13 +116,18 @@ func (src RedisSource) Response(req *ocsp.Request) ([]byte, http.Header, error) 
 	defer redisConn.Close()
 
 	// Search for OCSP response body in redis using sha1(AKI+SerialNumber) as key.
-	resp, err := redisConn.Do("GET", sha1.Sum(append(req.IssuerKeyHash, req.SerialNumber.Bytes()...)))
+	resp, err := redis.Bytes(redisConn.Do("GET", sha1.Sum(append(req.IssuerKeyHash, req.SerialNumber.Bytes()...))))
+
+	if err == redis.ErrNil || len(resp) == 0 {
+		return nil, nil, ErrNotFound
+	}
+
 	if err != nil {
 		log.Errorf("Error obtaining OCSP response from redis: %s", err)
 		return nil, nil, fmt.Errorf("failed to obtain OCSP response from redis: %s", err)
 	}
 
-	return resp.([]byte), nil, nil
+	return resp, nil, nil
 }
 
 // An InMemorySource is a map from serialNumber -> der(response)

commit 5dada2e8f56f3bf2efdfef70849276fa403c09a0

Enhanced handling of OCSP database communication errors
CFSSL returns HTTP 500 + OCSP internalerror on db connection errors and
HTTP 200 + OCSP internalerror on ocsp response parsing error which seems
inconsistent and too heavy - according to RFC 2560 internalError means
"the OCSP responder reached an inconsistent internal state". tryLater seems
to be better option for situations like SQL/redis restart during OCSP query.

This mod changes ocspserve responses in case of internal problems (like
DB/redis communication errors or OCSP response parsing problems)
from internalError to tryLater and does not use HTTP 500 (ugly results
in "openssl ocsp") but HTTP 200 (because ocsp generation errors are not
in HTTP layer and rfc6960#appendix-A does not require to set HTTP status
accroding to OCSP response status).

This mod also fixes errors returned in case of redis communication failures
(was same status as not found which is different situation).

Fixes: 5a716b1360abb09b245572fa477caf902185dfc6
Author-Change-Id: IB#1095303

ocsp/responder.go
@@ -118,7 +118,7 @@ func (src RedisSource) Response(req *ocsp.Request) ([]byte, http.Header, error) 
 	// Search for OCSP response body in redis using sha1(AKI+SerialNumber) as key.
 	resp, err := redis.Bytes(redisConn.Do("GET", sha1.Sum(append(req.IssuerKeyHash, req.SerialNumber.Bytes()...))))
 
-	if err == redis.ErrNil || len(resp) == 0 {
+	if err == redis.ErrNil {
 		return nil, nil, ErrNotFound
 	}
 
@@ -429,6 +429,7 @@ func (rs Responder) ServeHTTP(response http.ResponseWriter, request *http.Reques
 		if err == ErrNotFound {
 			log.Infof("No response found for request: serial %x, request body %s",
 				ocspRequest.SerialNumber, b64Body)
+			response.WriteHeader(http.StatusOK)
 			response.Write(unauthorizedErrorResponse)
 			if rs.stats != nil {
 				rs.stats.ResponseStatus(ocsp.Unauthorized)
@@ -437,10 +438,10 @@ func (rs Responder) ServeHTTP(response http.ResponseWriter, request *http.Reques
 		}
 		log.Infof("Error retrieving response for request: serial %x, request body %s, error: %s",
 			ocspRequest.SerialNumber, b64Body, err)
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write(internalErrorErrorResponse)
+		response.WriteHeader(http.StatusOK)
+		response.Write(tryLaterErrorResponse)
 		if rs.stats != nil {
-			rs.stats.ResponseStatus(ocsp.InternalError)
+			rs.stats.ResponseStatus(ocsp.TryLater)
 		}
 		return
 	}
@@ -449,9 +450,10 @@ func (rs Responder) ServeHTTP(response http.ResponseWriter, request *http.Reques
 	if err != nil {
 		log.Errorf("Error parsing response for serial %x: %s",
 			ocspRequest.SerialNumber, err)
-		response.Write(internalErrorErrorResponse)
+		response.WriteHeader(http.StatusOK)
+		response.Write(tryLaterErrorResponse)
 		if rs.stats != nil {
-			rs.stats.ResponseStatus(ocsp.InternalError)
+			rs.stats.ResponseStatus(ocsp.TryLater)
 		}
 		return
 	}