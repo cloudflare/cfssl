cloudflare/cfssl

commit 29ac288bf84806fa4a4c756cfea4e4feff65200f

Added redis support in ocsprefresh and ocspserver and related bugfixes
This mod adds ocsprefresh and ocspserve parameter -redis for using redis server
as OCSP response storage. With this enhancement, one may configure replication OCSP
responses to many ocspserve instances using redis, without file/SQL replication
quirks. Redis has also more advantages: auto eviction of old responses (use noeviction
mode in redis to avoid valid responses eviction), easier updating OCPS reponses for
recently changed (created/revoked) certs.

There is one more parameter added to ocsprefresh: recent-changes-only; use it to limit
range of refreshed responses to recent changes only; i.e. you can execute

    ocsprefresh ... -recent-changes-only 10m -redis 127.0.0.1:6379

every 5min to update OCSP responses for certs changed (added/revoked) in last
10 minutes and execute full update (without recent-changes-only; more time and data to
replicate) before all previous OCSP responses expiry.

This mod fixes also two bugs:

* mysql DNS should specify timezone of local system (added &loc=Local) - without it
times saved in MariaDB (tested in Debian 10) are incorrect (expiry, revoked_at)

* cfssl ocsprefresh should handle multiple different AKIs in one certdb as in
https://github.com/cloudflare/cfssl/pull/956

Related: https://github.com/cloudflare/cfssl/pull/956
Author-Change-Id: IB#1095303

ocsp/responder.go
@@ -9,6 +9,7 @@ package ocsp
 
 import (
 	"crypto"
+	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
@@ -25,6 +26,7 @@ import (
 	"github.com/cloudflare/cfssl/certdb/dbconf"
 	"github.com/cloudflare/cfssl/certdb/sql"
 	"github.com/cloudflare/cfssl/log"
+	"github.com/gomodule/redigo/redis"
 	"github.com/jmhodges/clock"
 	"golang.org/x/crypto/ocsp"
 )
@@ -54,6 +56,73 @@ var (
 // extra headers you may return nil instead.
 type Source interface {
 	Response(*ocsp.Request) ([]byte, http.Header, error)
+}
+
+// RedisSource represents a source of OCSP responses backed by the redis service.
+type RedisSource struct {
+	RedisPool redis.Pool
+}
+
+// NewRedisSource creates a new RedisSource type with an associated redisPool.
+func NewRedisSource(redisPool redis.Pool) Source {
+	return RedisSource{
+		RedisPool: redisPool,
+	}
+}
+
+// NewSourceFromRedis connects to specified redis server and
+// initializes connection pool for use with the OCSP responder.
+func NewSourceFromRedis(address string) (Source, error) {
+	redisPool := redis.Pool{
+		MaxIdle:     100,
+		MaxActive:   1000, // max number of connections
+		IdleTimeout: 180 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			dialOptions := []redis.DialOption{
+				redis.DialReadTimeout(time.Duration(3) * time.Second),
+				redis.DialWriteTimeout(time.Duration(3) * time.Second),
+				redis.DialConnectTimeout(time.Duration(5) * time.Second),
+			}
+
+			c, err := redis.Dial("tcp", address, dialOptions...)
+			if err != nil {
+				return nil, err
+			}
+
+			return c, nil
+		},
+	}
+	src := NewRedisSource(redisPool)
+	return src, nil
+}
+
+// Response implements cfssl.ocsp.responder.Source, which returns the
+// OCSP response from redis.
+func (src RedisSource) Response(req *ocsp.Request) ([]byte, http.Header, error) {
+
+	// Check if all required data is available.
+	if req == nil {
+		return nil, nil, errors.New("called with nil request")
+	}
+	if req.IssuerKeyHash == nil {
+		return nil, nil, errors.New("request contains no AKI")
+	}
+	if req.SerialNumber == nil {
+		return nil, nil, errors.New("request contains no cert serial number")
+	}
+
+	// Get redis connection from the pool and return it to the pool at the end.
+	redisConn := src.RedisPool.Get()
+	defer redisConn.Close()
+
+	// Search for OCSP response body in redis using sha1(AKI+SerialNumber) as key.
+	resp, err := redisConn.Do("GET", sha1.Sum(append(req.IssuerKeyHash, req.SerialNumber.Bytes()...)))
+	if err != nil {
+		log.Errorf("Error obtaining OCSP response from redis: %s", err)
+		return nil, nil, fmt.Errorf("failed to obtain OCSP response from redis: %s", err)
+	}
+
+	return resp.([]byte), nil, nil
 }
 
 // An InMemorySource is a map from serialNumber -> der(response)

certdb/mysql/dbconf.yml
@@ -1,15 +1,15 @@
 development:
   driver: mysql
-  open: root@tcp(localhost:3306)/certdb_development?parseTime=true
+  open: root@tcp(localhost:3306)/certdb_development?parseTime=true&loc=Local
 
 test:
   driver: mysql
-  open: root@tcp(localhost:3306)/certdb_test?parseTime=true
+  open: root@tcp(localhost:3306)/certdb_test?parseTime=true&loc=Local
 
 staging:
   driver: mysql
-  open: root@tcp(localhost:3306)/certdb_staging?parseTime=true
+  open: root@tcp(localhost:3306)/certdb_staging?parseTime=true&loc=Local
 
 production:
   driver: mysql
-  open: root@tcp(localhost:3306)/certdb_production?parseTime=true
+  open: root@tcp(localhost:3306)/certdb_production?parseTime=true&loc=Local

certdb/sql/database_accessor.go
@@ -34,6 +34,10 @@ SELECT %s FROM certificates
 SELECT %s FROM certificates
 	WHERE (CURRENT_TIMESTAMP < expiry AND authority_key_identifier = ?);`
 
+	selectAllUnexpiredRecentChangesOnlyByAKISQL = `
+SELECT %s FROM certificates
+	WHERE (CURRENT_TIMESTAMP < expiry AND authority_key_identifier = ? AND (created_at >= ?) OR (revoked_at >= ?));`
+
 	selectAllRevokedAndUnexpiredWithLabelSQL = `
 SELECT %s FROM certificates
 	WHERE CURRENT_TIMESTAMP < expiry AND status='revoked' AND ca_label= ?;`
@@ -168,6 +172,21 @@ func (d *Accessor) GetUnexpiredCertificatesByAKI(aki string) (crs []certdb.Certi
 	}
 
 	err = d.db.Select(&crs, fmt.Sprintf(d.db.Rebind(selectAllUnexpiredByAKISQL), sqlstruct.Columns(certdb.CertificateRecord{})), aki)
+	if err != nil {
+		return nil, wrapSQLError(err)
+	}
+
+	return crs, nil
+}
+
+// GetUnexpiredCertificatesRecentChangesOnlyByAKI gets from db all unexpired certificates changed not earlier than specified age, filtered by AKI.
+func (d *Accessor) GetUnexpiredCertificatesRecentChangesOnlyByAKI(aki string, oldestChangeTimestamp time.Time) (crs []certdb.CertificateRecord, err error) {
+	err = d.checkDB()
+	if err != nil {
+		return nil, err
+	}
+
+	err = d.db.Select(&crs, fmt.Sprintf(d.db.Rebind(selectAllUnexpiredRecentChangesOnlyByAKISQL), sqlstruct.Columns(certdb.CertificateRecord{})), aki, oldestChangeTimestamp, oldestChangeTimestamp)
 	if err != nil {
 		return nil, wrapSQLError(err)
 	}

cli/config.go
@@ -69,7 +69,9 @@ type Config struct {
 	AKI               string
 	DBConfigFile      string
 	CRLExpiration     time.Duration
-	Disable     	  string
+	Disable           string
+	Redis             string
+	RecentChangesOnly time.Duration
 }
 
 // registerFlags defines all cfssl command flags and associates their values with variables.
@@ -132,6 +134,8 @@ func registerFlags(c *Config, f *flag.FlagSet) {
 	f.DurationVar(&c.CRLExpiration, "expiry", 7*helpers.OneDay, "time from now after which the CRL will expire (default: one week)")
 	f.IntVar(&log.Level, "loglevel", log.LevelInfo, "Log level (0 = DEBUG, 5 = FATAL)")
 	f.StringVar(&c.Disable, "disable", "", "endpoints to disable")
+	f.StringVar(&c.Redis, "redis", "", "redis service address and port i.e. 127.0.0.1:6379")
+	f.DurationVar(&c.RecentChangesOnly, "recent-changes-only", 0, "process only certificates created or revoked no earlier than specified duration i.e. 1h (default: no limit)")
 }
 
 // RootFromConfig returns a universal signer Root structure that can

cli/ocsprefresh/ocsprefresh.go
@@ -2,30 +2,33 @@
 package ocsprefresh
 
 import (
+	"crypto/sha1"
 	"encoding/hex"
 	"errors"
 	"time"
 
+	"github.com/cloudflare/cfssl/certdb"
 	"github.com/cloudflare/cfssl/certdb/dbconf"
 	"github.com/cloudflare/cfssl/certdb/sql"
 	"github.com/cloudflare/cfssl/cli"
 	"github.com/cloudflare/cfssl/helpers"
 	"github.com/cloudflare/cfssl/log"
 	"github.com/cloudflare/cfssl/ocsp"
+	"github.com/gomodule/redigo/redis"
 )
 
 // Usage text of 'cfssl ocsprefresh'
 var ocsprefreshUsageText = `cfssl ocsprefresh -- refreshes the ocsp_responses table
-with new OCSP responses for all known unexpired certificates
+with new OCSP responses for all known unexpired certificates (with recently changed only as option).
 
 Usage of ocsprefresh:
-        cfssl ocsprefresh -db-config db-config -ca cert -responder cert -responder-key key [-interval 96h]
+        cfssl ocsprefresh -db-config db-config -ca cert -responder cert -responder-key key [-interval 96h] [-recent-changes-only 5min] [-redis host:port]
 
 Flags:
 `
 
 // Flags of 'cfssl ocsprefresh'
-var ocsprefreshFlags = []string{"ca", "responder", "responder-key", "db-config", "interval"}
+var ocsprefreshFlags = []string{"ca", "responder", "responder-key", "db-config", "interval", "recent-changes-only", "redis"}
 
 // ocsprefreshMain is the main CLI of OCSP refresh functionality.
 func ocsprefreshMain(args []string, c cli.Config) error {
@@ -65,9 +68,39 @@ func ocsprefreshMain(args []string, c cli.Config) error {
 		return err
 	}
 	dbAccessor := sql.NewAccessor(db)
-	certs, err := dbAccessor.GetUnexpiredCertificatesByAKI(hex.EncodeToString(CACert.SubjectKeyId))
-	if err != nil {
-		return err
+
+	var certs []certdb.CertificateRecord
+
+	if c.RecentChangesOnly != 0 {
+		oldestChangeTimestamp := time.Now().Add(-c.RecentChangesOnly).UTC()
+		c, err := dbAccessor.GetUnexpiredCertificatesRecentChangesOnlyByAKI(hex.EncodeToString(CACert.SubjectKeyId), oldestChangeTimestamp)
+		if err != nil {
+			return err
+		}
+		certs = c
+	} else {
+		c, err := dbAccessor.GetUnexpiredCertificatesByAKI(hex.EncodeToString(CACert.SubjectKeyId))
+		if err != nil {
+			return err
+		}
+		certs = c
+	}
+
+	// Open connection to redis service if redis flag present.
+	var redisConn redis.Conn
+	if c.Redis != "" {
+		dialOptions := []redis.DialOption{
+			redis.DialReadTimeout(time.Duration(10) * time.Second),
+			redis.DialWriteTimeout(time.Duration(10) * time.Second),
+			redis.DialConnectTimeout(time.Duration(30) * time.Second),
+		}
+
+		if redisConn, err = redis.Dial("tcp", c.Redis, dialOptions...); err != nil {
+			log.Critical("Unable to connect to redis service: ", err)
+			return err
+		}
+
+		defer redisConn.Close()
 	}
 
 	// Set an expiry timestamp for all certificates refreshed in this batch
@@ -95,10 +128,20 @@ func ocsprefreshMain(args []string, c cli.Config) error {
 			return err
 		}
 
-		err = dbAccessor.UpsertOCSP(cert.SerialNumber.String(), hex.EncodeToString(cert.AuthorityKeyId), string(resp), ocspExpiry)
-		if err != nil {
-			log.Critical("Unable to save OCSP response: ", err)
-			return err
+		// Store signed response in redis if enabled; store in SQL db otherwise.
+		if redisConn != nil {
+			// Use sha1 of cert AKI+SerialNumber for smaller and faster keys in redis; set
+			// item expiry in redis (in seconds) same as for OCSP response.
+			if _, err := redisConn.Do("SET", sha1.Sum(append(cert.AuthorityKeyId, cert.SerialNumber.Bytes()...)), resp, "EX", int(c.Interval.Seconds())); err != nil {
+				log.Critical("Unable to save OCSP response in redis: ", err)
+				return err
+			}
+		} else {
+			err = dbAccessor.UpsertOCSP(cert.SerialNumber.String(), hex.EncodeToString(cert.AuthorityKeyId), string(resp), ocspExpiry)
+			if err != nil {
+				log.Critical("Unable to save OCSP response in SQL database: ", err)
+				return err
+			}
 		}
 	}
 

cli/ocspserve/ocspserve.go
@@ -12,16 +12,16 @@ import (
 )
 
 // Usage text of 'cfssl serve'
-var ocspServerUsageText = `cfssl ocspserve -- set up an HTTP server that handles OCSP requests from either a file or directly from a database (see RFC 5019)
+var ocspServerUsageText = `cfssl ocspserve -- set up an HTTP server that handles OCSP requests from either a file or directly from a database or from redis service (see RFC 5019)
 
   Usage of ocspserve:
-          cfssl ocspserve [-address address] [-port port] [-responses file] [-db-config db-config]
+          cfssl ocspserve [-address address] [-port port] [-responses file] [-db-config db-config] [-redis host:port]
 
   Flags:
   `
 
 // Flags used by 'cfssl serve'
-var ocspServerFlags = []string{"address", "port", "responses", "db-config"}
+var ocspServerFlags = []string{"address", "port", "responses", "db-config", "redis"}
 
 // ocspServerMain is the command line entry point to the OCSP responder.
 // It sets up a new HTTP server that responds to OCSP requests.
@@ -32,7 +32,13 @@ func ocspServerMain(args []string, c cli.Config) error {
 		return errors.New("argument is provided but not defined; please refer to the usage by flag -h")
 	}
 
-	if c.Responses != "" {
+	if c.Redis != "" {
+		s, err := ocsp.NewSourceFromRedis(c.Redis)
+		if err != nil {
+			return errors.New("unable to connect to redis service")
+		}
+		src = s
+	} else if c.Responses != "" {
 		s, err := ocsp.NewSourceFromFile(c.Responses)
 		if err != nil {
 			return errors.New("unable to read response file")
@@ -46,7 +52,7 @@ func ocspServerMain(args []string, c cli.Config) error {
 		src = s
 	} else {
 		return errors.New(
-			"no response file or db-config provided, please set the one of these using either -responses or -db-config flags",
+			"no response file nor db/redis config provided, please set the one of these using either -responses or -db-config or -redis flags",
 		)
 	}
 

commit 5a716b1360abb09b245572fa477caf902185dfc6

Fixed handling empty responses from redis
Fixed panics when OCSP response is was not found in redis.

Fixes: 29ac288bf84806fa4a4c756cfea4e4feff65200f
Author-Change-Id: IB#1095303

ocsp/responder.go
@@ -116,13 +116,18 @@ func (src RedisSource) Response(req *ocsp.Request) ([]byte, http.Header, error) 
 	defer redisConn.Close()
 
 	// Search for OCSP response body in redis using sha1(AKI+SerialNumber) as key.
-	resp, err := redisConn.Do("GET", sha1.Sum(append(req.IssuerKeyHash, req.SerialNumber.Bytes()...)))
+	resp, err := redis.Bytes(redisConn.Do("GET", sha1.Sum(append(req.IssuerKeyHash, req.SerialNumber.Bytes()...))))
+
+	if err == redis.ErrNil || len(resp) == 0 {
+		return nil, nil, ErrNotFound
+	}
+
 	if err != nil {
 		log.Errorf("Error obtaining OCSP response from redis: %s", err)
 		return nil, nil, fmt.Errorf("failed to obtain OCSP response from redis: %s", err)
 	}
 
-	return resp.([]byte), nil, nil
+	return resp, nil, nil
 }
 
 // An InMemorySource is a map from serialNumber -> der(response)