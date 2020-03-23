0870f5f9932645b1d5b26c7adcaaf68f777dc717
Sofía Celi <cherenkovd69@gmail.com>
2020-03-23 01:38:19 -0500

    Fix 'TestNewMutualTLSServer' test for go1.14 #1075

--- api/client/client_test.go
+++ api/client/client_test.go
@@ -97,7 +97,7 @@ func TestNewMutualTLSServer(t *testing.T) {
 	if err == nil {
 		t.Fatalf("expected error with sign function")
 	}
-	if !strings.Contains(err.Error(), "Post https://nohost:8888/api/v1/cfssl/sign: dial tcp: lookup nohost") {
+	if !(strings.Contains(err.Error(), "Post")) && !(strings.Contains(err.Error(), "https://nohost:8888/api/v1/cfssl/sign")) && !(strings.Contains(err.Error(), "dial tcp: lookup nohost: no such host")) {
 		t.Fatalf("no error message %v", err)
 	}
 }
